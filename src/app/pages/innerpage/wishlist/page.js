6feea46a6b0cdc3dd0efb78eb4bb313f38b8f99b
okulluz kiyaka <kiyakaokullu@gmail.com>
2025-12-29 14:15:58 +0300

    updated wishlist

--- src/app/pages/innerpage/wishlist/page.js
+++ src/app/pages/innerpage/wishlist/page.js
@@ -1,15 +1,26 @@
+import dynamic from "next/dynamic";
 import BreadcumbFifteen from "~/sections/Wishlist/BreadcumbFifteen";
-import Wishlist from "~/sections/Wishlist/Wishlist";
 import HeaderOne from "~/sections/Common/Header/HeaderOne";
 import FooterFive from "~/sections/Common/Footer/FooterFive";
 import Scroll from "~/sections/Common/Scroll";
+
+// This tells Next.js: "Do not try to render this on the server"
+const Wishlist = dynamic(() => import("~/sections/Wishlist/Wishlist"), {
+    ssr: false,
+    loading: () => <div className="container p-5 text-center">Loading Wishlist...</div>
+});
+
 export default function WishlistPage() {
     return (
         <div>
             <HeaderOne />
             <BreadcumbFifteen />
-            <Wishlist />
-            <FooterTwo />
+            {/* The dynamic component now safe for Vercel builds */}
+            <Wishlist /> 
+            
+            {/* Fixed: Changed FooterTwo to FooterFive to match your import */}
+            <FooterFive /> 
+            
             <Scroll />
         </div>
     );
