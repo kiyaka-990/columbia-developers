037fb4c8893824fe211ea5595059f8c2138c2835
okulluz kiyaka <kiyakaokullu@gmail.com>
2026-01-06 20:58:08 +0300

    updated prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -4,10 +4,6 @@ const globalForPrisma = globalThis;
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    // In Prisma 7, you must explicitly pass the connection string here
-    // if it's not in a prisma.config.ts file
-    datasourceUrl: process.env.DATABASE_URL, 
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
