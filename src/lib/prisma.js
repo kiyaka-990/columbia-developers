729dd8eda943895c8f3e8fc76dd3aab5450fd562
okulluz kiyaka <kiyakaokullu@gmail.com>
2026-01-06 20:41:21 +0300

    updated prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export { prisma };
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
